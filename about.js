18a28b0a359307738f931656ba30d65b73cb3fe0
Sashtekar <153296228+Sashtekar@users.noreply.github.com>
2023-12-08 07:05:33 -0800

    Add files via upload

---
+++ about.js
@@ -0,0 +1,10 @@
+const about=()=>{
+    return(
+        <div>
+            <h1> About page here!</h1>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipicing elit. Fugit, modi!
+            </p>
+        </div>
+    );
+    }
